bce2ec5eb14b0e7ae25e56f817676ddfde4dbcb2
Enrique <okay.enrique@gmail.com>
2022-11-17 14:53:50 -0500

    lab complete

---
+++ models/character.model.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const characterSchema = new Schema({
+  name: String,
+  occupation: String,
+  weapon: String,
+  debt: Boolean
+});
+
+const Character = model("Character", characterSchema);
+
+module.exports = Character;
--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const axios = require("axios");
 
+const Character = require('../models/character.model');
+
 /* GET home page */
 router.get("/characters", (req, res, next) => {
     axios.get("https://ih-crud-api.herokuapp.com/characters")
@@ -11,6 +13,11 @@ router.get("/characters", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+router.get('/characters/create', (req, res, next) => {
+    res.render('characters/create-character.hbs')
+});
+
+
 
 router.get("/characters/:id", (req, res, next) => {
     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
@@ -20,6 +27,57 @@ router.get("/characters/:id", (req, res, next) => {
     })
     .catch(err => console.error(err))
 });
+
+router.get('/characters/:id/edit', (req, res, next) => {
+    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then(responseFromAPI => {
+        res.render('characters/edit-character.hbs', { character: responseFromAPI.data })
+    })
+    .catch(err => console.log(err))
+});
+
+router.post('/characters/create', (req, res, next) => {
+
+    const createCharacter = { 
+        name: req.body.name,
+        occupation: req.body.occupation,
+        weapon: req.body.weapon,
+        debt: req.body.debt
+ };
+
+    axios.post('https://ih-crud-api.herokuapp.com/characters', createCharacter)
+    .then(responseFromAPI => {
+        Character.create({responseFromAPI})
+        res.redirect('/characters')
+    })
+    .catch(err => console.log(err));
+})
+
+router.post('/characters/:id/update', (req, res, next) => {
+
+    const createCharacter = { 
+        name: req.body.name,
+        occupation: req.body.occupation,
+        weapon: req.body.weapon,
+        debt: req.body.debt
+ };
+
+    axios.put(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`, createCharacter)
+    .then(responseFromAPI => {
+
+        res.redirect(`/characters/${req.params.id}`)
+    })
+    .catch(err => console.log(err));
+});
+
+router.post('/characters/:id/delete', (req, res, next) => {
+    axios.delete(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then(responseFromAPI => {
+        res.redirect('/characters')
+    })
+    .catch(err => console.log(err));
+});
+
 
 module.exports = router;
 
